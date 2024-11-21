f945293aa8c020292a65ebf32c40cfd648d1d3ed
faari-joye <faarijoye@gmail.com>
2024-11-21 08:18:39 +0100

    staging initial commits

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const signUp = () => {
+  return (
+    <div>SIGN-UP!!</div>
+  )
+}
+
+export default signUp
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <main>home</main>
+  )
+}
+
+export default home
